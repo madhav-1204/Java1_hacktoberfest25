bc9d993d2ade4b5b7128d7ec0dfdc0e9318e698c
Ch-MANASA19 <manasachimata19@gmail.com>
2025-09-30 18:56:15 +0530

    Update methoddemo.java

--- methoddemo.java
+++ methoddemo.java
@@ -2,18 +2,15 @@
 ----------------------------------------------------
 class A
 {
-int l=10,b=20;
-int area()
-{
-return l*b;
-}
+
 }
 class methoddemo2
 {
 public static void main(String args[])
 {
 A a1=new A();
-int r=a1.area();
+
 System.out.println("The area is:"+r);
 }
+
 }
